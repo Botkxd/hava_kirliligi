b9f2d4d56a2d8296ed180d109feb563b0e9e6908
Botkxd <145799758+Botkxd@users.noreply.github.com>
2023-10-30 19:01:17 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yaptık')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send(f'Merhaba! Bana hava kirliliği hakkında istdiğini sor!')
+    elif  message.content.startswith('hava kirliliği nedir'):
+        cevap1="""Hava kirliliği, canlıların sağlığını olumsuz yönde etkileyen ve havadaki yabancı maddelerin, normalin üzerinde miktar ve yoğunluğa ulaşmasıdır. Hava kirliliği; havada katı, sıvı ve gaz şeklindeki yabancı maddelerin insan sağlığına, canlı hayatına ve ekolojik dengeye zarar verecek miktar, yoğunluk ve uzun sürede atmosferde bulunmasıdır. İnsanların çeşitli faaliyetleri sonucu meydana gelen üretim ve tüketim aktiviteleri sırasında ortaya çıkan atıklarla hava tabakası kirlenerek, yeryüzündeki canlı hayatını olumsuz yönde etkilemektedir.Hava kirliliği yüzünden her sene 7 milyon kişi ölmekte."""
+        await message.channel.send(cevap1)
+    elif  message.content.startswith("Hava kirliliğini önlemek için alınabilecek tedbirler?"):
+        cevap2 = """
+        Hükûmet
+Kentlerde kömür kullanımı yasaklanmalı
+Kömür santraller kapatılmalı
+İnsanlar toplu taşımacılığa özendirilmeli, elektrikli toplu ulaşım araçlarında kullanılması yaygınlaştırılmalı
+Kentlerde arabaların egzozlarından kaynaklanan kirliliğin azaltılması için önlemler alınmalıdır.
+Karbon piyasası ya da vergisi yaratılmalı
+Sanayi tesislerinin bacalarına filtre takılması sağlanmalı, ayrıca sanayi kuruluşları yer seçimi düzenli yapılmalı,
+Günlük Hayatta
+Kentlerde kömür kullanılmamalı
+Kentlerde ya yürümeye ya bisiklet ya da elektrikli araçlar kullanılmalı"""
+        await message.channel.send(cevap2)
+
+    elif  message.content.startswith("hava kirliliğinin sonuçları nelerdir?"):
+        cevap3 = """Kirli hava, insanlarda solunum yolu hastalıklarının artmasına sebep olmaktadır.[7] Örneğin; kurşunun kan hücrelerinin gelişmesini ve olgunlaşmasını engellediği, kanda ve idrarda birikerek sağlığı olumsuz yönde etkilediği, karbon monoksit (CO)'in ise, kandaki hemoglobin ile birleşerek oksijen taşınmasını aksattığı bilinmektedir. Bununla birlikte kükürt dioksit (SO2)'in, üst solunum yollarında keskin, boğucu ve tahriş edici etkileri vardır. Özellikle duman akciğerden alveollere kadar girerek olumsuz etki yapmaktadır. Cilt hastalıkları, saç dökülmesi, akciğer hastalıkları ve kansere yol açtığı somut bir gerçektir.[8] Ayrıca kükürt dioksit ve ozon bitkiler için zararlı olup; özellikle ozon, ürün kayıplarına sebep olmakta ve ormanlara zarar vermektedir. Kirli hava kilo yapar [9] ve genleri etkiler.[10] Azot dioksit(NO2) çocuklarda astım ve akciğer hastalıklarına yol açabiliyor.[11]
+
+Sanayi, endüstri ve ısınmada kullanılan fosil yakıtlar ile ormanların tahribi ve arazi değişmesi sonucu, atmosferdeki karbondioksit miktarının %5 oranında arttığı tespit edilmiştir. Bunun ise küresel ısınmaya yol açabileceği öngörülmektedir.
+
+300 milyon çocuk 'zehir soluyor'."""
+        await message.channel.send(cevap3)
+        
+client.run("")
